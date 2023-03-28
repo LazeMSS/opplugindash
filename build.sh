66332a5a463e718450e3a316a6888cc3ccc2f4bc
LazeMSS <github@jemi.dk>
2023-03-28 09:18:52 +0200

    First commit

---
+++ build.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+# output data dir
+dataDir="./www/data/"
+statsFile="${dataDir}stats.json"
+pluginsFile="${dataDir}plugins.json"
+
+# user config file
+configFile="./config.json"
+
+pluginSrc="https://plugins.octoprint.org/plugins.json"
+statsSrc="https://data.octoprint.org/export/plugin_stats_30d.json"
+
+if [[ -z "${curUser}" ]]; then
+	curUser=$USER
+fi
+now=$(date +'%Y-%m-%d-%s')
+
+errormsg(){
+	echo -e "\e[0;31m"Error: "\e[0m""$1"
+}
+
+echo "Running as user (GitHub repo owner): $curUser"
+
+# Config file found
+if [ ! -f $configFile ]; then
+	errormsg "Unable to find plugin configfile: $configFile"
+	exit 1
+fi
+
+# check for jq
+if ! command -v jq &> /dev/null; then
+	apt-get install jq
+fi
+
+# check dir
+if [ ! -d "$dataDir" ]; then
+	echo "Creating folder $dataDir"
+	mkdir -p "$dataDir"
+fi
+
+# Check if we have the right user
+makeNewUser=true
+if [ -f "repo_owner" ]; then
+	read -r fileRepoOwner < repo_owner
+	if [ "$fileRepoOwner" == "$curUser" ]; then
+		makeNewUser=false
+	fi
+fi
+
+# Reset on new user
+if $makeNewUser; then
+	echo "New user running - will clean stats!"
+
+	mv "$statsFile" "${statsFile}.bak" 2> /dev/null
+	mv "$pluginsFile" "${pluginsFile}.bak" 2> /dev/null
+
+	# assign the new user
+	echo "$curUser" > repo_owner
+fi
+
+# Build new files
+if [ ! -f "$statsFile" ]; then
+	echo "{}" > $statsFile
+fi
+if [ ! -f "$pluginsFile" ]; then
+	echo "{}" > $pluginsFile
+fi
+
+
+mapfile -t plugins < <(jq -cr '.[]' $configFile)
+if [ ${#plugins[@]} -eq 0 ]; then
+	errormsg "Zero entries found in $configFile"
+	exit 1
+fi
+echo "Found ${#plugins[@]} plugin(s) in $configFile"
+
+
+# get stats
+curl -sS $pluginSrc --output plugins.json
+curl -sS $statsSrc --output stats.json
+
+statsTime=$(jq -cr '._generated' stats.json 2>&1)
+statsTime=$(date -d "$statsTime" +"%Y-%m-%d")
+
+# for pluginid in "${plugins[@]}"
+# do
+# 	echo "Gettings generic stats for $pluginid"
+# 	if grep -q '"id": "'"$pluginid"'"' plugins.json; then
+
+# 		# build generic stats/github stats
+# 		jq '.[]| select(.id=="'"$pluginid"'")  | {"stats":.stats, "issues":.github|.issues , "stars":.github.stars} | {"'"${pluginid}"'" : {"'"$now"'": .}}' plugins.json > tmp_stats.json
+# 		jq -s '.[0] * .[1]' tmp_stats.json $pluginsFile > tmp_merge.json
+# 		mv tmp_merge.json $pluginsFile
+
+# 		# Build version stats
+# 		jq '.plugins.'${pluginid} stats.json | jq -c '{"'"${pluginid}"'" : {"'"$statsTime"'": .}}' > tmp_stats.json
+# 		jq -s '.[0] * .[1]' tmp_stats.json $statsFile > tmp_merge.json
+# 		mv tmp_merge.json $statsFile
+# 	else
+# 		errormsg "Could not locate $pluginid in $pluginSrc"
+# 	fi
+# done
+
+configMap=$(jq -c '[ .[] | {key: (.), value: null} ] | from_entries' config.json)
+
+# No checking whether config values exist in plugins.json here
+
+# build generic stats/github stats
+jq --argjson config "$configMap" --arg now "$now" --slurpfile result "$pluginsFile" '
+	[
+		.[] | select(.id | in($config)) |
+		{
+			(.id): {
+				($now): {
+						"stats": (.stats),
+						"issues": (.github.issues),
+						"stars": (.github.stars)
+						}
+			}
+		}
+	] | reduce .[] as $add ($result[0]; . * $add)' plugins.json > tmp_merge.json
+mv tmp_merge.json "$pluginsFile"
+
+# Build version stats
+jq --argjson config "$configMap" --arg statsTime "$statsTime" --slurpfile result "$statsFile" '
+	[
+	.plugins | to_entries | .[] | select(.key | in($config)) |
+		{
+			(.key): {
+				($statsTime): (.value)
+			}
+	}
+	] | reduce .[] as $add ($result[0]; . * $add)' stats.json > tmp_merge.json
+mv tmp_merge.json $statsFile
+
+rm plugins.json stats.json tmp_* 2> /dev/null
+
+# copy config file to the website
+cp $configFile ${dataDir}config.json
---
+++ www/main.js
@@ -0,0 +1,212 @@
+/*
+Todo:
+	Action for downloading stats daily
+		Shell https://stedolan.github.io/jq/
+
+	Cleanup graph
+
+	Drilldown stats for:
+		installs, version and github stats pr. day
+*/
+
+//paths to json files
+const configFile = "data/config.json"
+
+// Store data for faster builds
+var myPlugins = [];
+var stats30d = {};
+var pluginsinfo = {};
+var localPlugins = {};
+// Graph resize timer
+var resizeTimer = null;
+
+function toogleDarkMOde() {
+	$('i.modeicon').toggleClass('bi-moon bi-sun');
+	let newMode = ($('html').attr("data-bs-theme") == "dark") ? "light" : "dark"
+	$('html').attr("data-bs-theme", newMode);
+}
+
+// ajax quick handler - needs an error handler
+function xhrJson(url, callbackOk) {
+	let xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			callbackOk(JSON.parse(this.responseText));
+		}
+	};
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+}
+
+// load and refresh data
+function loadData(refresh) {
+	if (refresh) {
+		alert("WIP");
+		return;
+		$('#fetchSpinner').removeClass('d-none');
+	}
+
+
+	// Get main config
+	xhrJson(configFile, function(data) {
+		myPlugins = data;
+		// Get primary plugin info for names etc
+		xhrJson("https://plugins.octoprint.org/plugins.json", function(data) {
+			pluginsinfo = data;
+
+			// Get the 30d stats / should be removed and replace with history stats only or as fallback
+			xhrJson("https://data.octoprint.org/export/plugin_stats_30d.json", function(data) {
+				stats30d = data;
+				// Get history data and handle
+				xhrJson("data/plugins.json", function(data) {
+					localPlugins = data;
+					buildStats();
+				})
+			});
+		});
+	});
+}
+
+function buildStats() {
+	let tempPInfo = {};
+	pluginsinfo.forEach(function(item) {
+		let idLc = item.id.toLowerCase();
+		if (myPlugins.includes(item.id) || myPlugins.includes(idLc)) {
+			tempPInfo[idLc] = item;
+		}
+	});
+	pluginsinfo = tempPInfo;
+	myPlugins.forEach(buildPluginStat);
+	$('#mainSpinner').remove();
+	$('#mainDash,#fetchSpinner').removeClass('d-none');
+	$('#fetchSpinner').addClass('d-none');
+}
+
+function buildPluginStat(item) {
+	item = item.toLowerCase();
+	if (item in stats30d.plugins) {
+		// Basic template field from data
+		let template = document.querySelector('#mainDashTemplate');
+		let clone = template.content.cloneNode(true);
+		// Find all plugin info data
+		let templateFiller = clone.querySelectorAll('[data-pinfo]');
+		templateFiller.forEach(function(curVal, curInd, listObj) {
+			// Lookup the value in the tree
+			let itemVal = resolvePath(pluginsinfo[item], curVal.getAttribute('data-pinfo'), null);
+			// Nothing to fil - then delete the template item
+			if (itemVal == null || itemVal === false) {
+				listObj[curInd].remove();
+				return;
+			}
+
+			// Set urls if a href tags
+			if (listObj[curInd].tagName == "A") {
+				listObj[curInd].setAttribute("href", itemVal.replace(/\/archive\/(.*)/, ''));
+			} else {
+				listObj[curInd].innerHTML = itemVal;
+			}
+		});
+
+
+		// build pie chart of versions numbers
+		let statKeys = [];
+		let statVals = [];
+		let tempStats = [];
+		for (const [key, value] of Object.entries(stats30d.plugins[item].versions)) {
+			tempStats.push({
+				'ver': key,
+				'val': value.instances
+			});
+		}
+		tempStats.sort(function(a, b) {
+			return b.val - a.val;
+		});
+		let newSet = tempStats.slice(0, 5)
+		let other = 0;
+		tempStats.slice(5).forEach(function(curVal) {
+			other += curVal.val;
+		});
+		if (other > 0) {
+			newSet.push({
+				"ver": "other",
+				"val": other
+			});
+		}
+		newSet.forEach(function(curVal) {
+			statKeys.push(curVal.ver);
+			statVals.push(curVal.val);
+		});
+		let canvas = clone.querySelectorAll("canvas[data-graph=\"versions\"]")[0];
+		canvas.setAttribute("id", "versiongraph_" + item);
+		let dataSet = {
+			labels: statKeys,
+			datasets: [{
+				label: 'Totals for ' + item,
+				data: statVals,
+				hoverOffset: 4
+			}]
+		};
+
+		// Build graph with install base
+		let canvasHis = clone.querySelectorAll("canvas[data-graph=\"history\"]")[0];
+		if (item in localPlugins) {
+			let hiskeys = [];
+			let hisval = [];
+			for (const [key, value] of Object.entries(localPlugins[item])) {
+				hiskeys.push(key);
+				hisval.push(value.stats.instances_month);
+			}
+			canvasHis.setAttribute("id", "histgraph_" + item);
+			var histDatSet = {
+				labels: hiskeys,
+				datasets: [{
+					label: "Total installbase",
+					data: hisval
+				}]
+			};
+		} else {
+			canvasHis.remove();
+			canvasHis = null;
+		}
+		// add the item
+		document.querySelector('#mainDash').appendChild(clone);
+		// Assign the graph
+		new Chart(document.querySelector("#versiongraph_" + item), {
+			type: 'pie',
+			data: dataSet
+		});
+		// Assign the graph
+		if (canvasHis != null) {
+			new Chart(document.querySelector("#histgraph_" + item), {
+				type: 'line',
+				data: histDatSet,
+				options: {
+					plugins: {
+						legend: {
+							position: "bottom"
+						}
+					}
+				}
+			});
+		}
+	}
+}
+
+// Search and object for a string path
+// https://stackoverflow.com/a/43849204
+const resolvePath = (object, path, defaultValue) => path.split('.').reduce((o, p) => o ? o[p] : defaultValue, object);
+
+// When ready lets load
+$(function() {
+	window.onresize = function() {
+		if (resizeTimer != null) {
+			clearTimeout(resizeTimer);
+		}
+		resizeTimer = setTimeout(function() {
+			for (let id in Chart.instances) {
+				Chart.instances[id].resize();
+			}
+		}, 100);
+	}
+	loadData(false);
+});
